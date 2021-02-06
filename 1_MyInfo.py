ff2ad5986c00b1b36906150997b264f77b964522
decchu14 <1decchu14@gmail.com>
2021-02-07 02:16:44 +0530

    adding code

--- 1_MyInfo.py
+++ 1_MyInfo.py
@@ -1,20 +1,20 @@
 import tweepy
 import MyAPIKeys  # just created a seperate python file to keep my keys so that i can just import it instead of typing it again and again
 
-
+# setting the auth
 auth = tweepy.OAuthHandler(MyAPIKeys.Api_Key, MyAPIKeys.Api_Key_Secret)
 auth.set_access_token(MyAPIKeys.Access_Token, MyAPIKeys.Access_Token_Secret)
-
 api = tweepy.API(auth)
 
 
 user = api.me()
-print(user.name)  # fetching twitter user name
+print(user.name)  # fetching my twitter user name
 
-print(user.screen_name)  # fetching twitter screen name
-print(user.followers_count)  # fetching the count of followers
+print(user.screen_name)  # fetching my twitter screen name
+print(user.followers_count)  # fetching my count of followers
 
-# this block fetches the tweets from the homeline
+# this block fetches the tweets from the my homeline
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
-    print(tweet.text)
+    tweet = tweet.text.encode('utf8')
+    print(tweet, "\n")
